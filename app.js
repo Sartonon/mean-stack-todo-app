a0afb8ff074ec46a1a3ae3dfa4a5c2cf36accb7e
Santeri Rusila <santtu_1993@hotmail.com>
2018-04-15 18:16:46 +0300

    port 4000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = require('./config');
 var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
 
-var port = process.env.PORT || 3000;
+var port = 4000;
 
 app.set('view engine', 'ejs');
 
